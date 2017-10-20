8f385eaf4e888dee94a09de806535ef5157bd12d
Jaret Binford <jaretbinford@gmail.com>
2017-10-20 13:35:47 -0400

    basketball keeper

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to baskethead
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject baskethead "0.0.0-SNAPSHOT"
+  :description "Fantasy Basketball stat keeper and analyzer"
+  :url "www.jaretbinford.com/basketball"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+                 [com.datomic/client-cloud "0.8.45"])
