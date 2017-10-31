a120f1367f0284e00385c2f62d72bb3c4695af1b
Jaret Binford <jaretbinford@gmail.com>
2017-10-30 22:15:56 -0400

    typo

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
-                 [com.datomic/client-cloud "0.8.45"])
+                 [com.datomic/client-cloud "0.8.45"]])
